fd0be7ce4e8a14f5a9d48f67b5c0be4f49a4b61e
E. R. C. Pereda <ercp42@gmail.com>
2016-09-26 22:28:37 -0300

    feat(component): Implement basic modal image functionality

--- src/ModalImage.js
+++ src/ModalImage.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import classnames from 'classnames';
 import styles from './ModalImage.css';
 
-const ModalImage = props => (
-  <div className={styles.component}>
-    {props.text}
-  </div>
-);
+class ModalImage extends React.Component {
+  constructor() {
+    super();
+
+    this.close = this.close.bind(this);
+  }
+
+  close() {
+    this.props.onRequestClose();
+  }
+
+  render() {
+    const modalClass = classnames(styles.modal, {
+      [styles.show]: this.props.isOpen
+    });
+
+    return (
+      <div className={modalClass}>
+        <span className={styles.close} onClick={this.close}>&times;</span>
+        <img className={styles.modalContent} src={this.props.img} alt="modal" />
+        <div className={styles.caption}>{this.props.caption}</div>
+      </div>
+    );
+  }
+}
 
 ModalImage.propTypes = {
-  text: React.PropTypes.string
+  isOpen: React.PropTypes.bool,
+  img: React.PropTypes.string,
+  caption: React.PropTypes.string,
+  onRequestClose: React.PropTypes.func
 };
 
 export default ModalImage;
--- src/main.js
+++ src/main.js
@@ -2,5 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ModalImage from './ModalImage';
 
-ReactDOM.render(<ModalImage text={'Hello World!!'} />, document.getElementById('app'));
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      modalIsOpen: false
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+
+  closeModal() {
+    this.setState({ modalIsOpen: false });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.openModal}>Open Modal</button>
+        <ModalImage
+          isOpen={this.state.modalIsOpen}
+          img="https://www.bing.com/az/hprichbg/rb/Castelmezzano_EN-US11750585825_1920x1080.jpg"
+          caption="Castelmezzano, Italy"
+          onRequestClose={this.closeModal}
+        />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
